4447121ddbdb0c82d6a04cbe7cb7652021428f4f
Maksim Ivanov <satansdeer@gmail.com>
2020-05-26 07:44:54 +0200

    Handle on submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm() 
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input ref={register} type="file" name="picture" />
+      <button>Submit</button>
+    </form>
   );
 }
 
